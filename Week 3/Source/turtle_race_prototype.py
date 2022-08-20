2401edf8182a0f438d1356d3c91531bcdc321f74
Pattarapark Chutisamoot <most.chuti@gmail.com>
2022-08-20 13:08:04 +0700

    Initial Codes for turtle_race_prototype.py is created

--- Week 3/Source/turtle_race_prototype.py
+++ Week 3/Source/turtle_race_prototype.py
@@ -14,6 +14,12 @@ racer_3 = turtle.Turtle()
 racer_4 = turtle.Turtle()
 racer_5 = turtle.Turtle()
 
+racer_1.shape("turtle")
+racer_2.shape("turtle")
+racer_3.shape("turtle")
+racer_4.shape("turtle")
+racer_5.shape("turtle")
+
 racer_1.hideturtle()
 racer_2.hideturtle()
 racer_3.hideturtle()
@@ -42,19 +48,49 @@ class DefineTurtle:
         self.speed = speed
         self.energy = energy
 
-    def turtle_placement(self):
+    def placement(self):
         if self.color == "black":
+            racer_1.penup()
+            racer_1.showturtle()
             racer_1.setpos(-500, 100)
-        if self.color == "blue":
-            racer_1.setpos(-500, 0)
-        if self.color == "green":
-            racer_1.setpos(-500, -100)
-        if self.color == "red":
-            racer_1.setpos(-500, 200)
-        if self.color == "grey":
-            racer_1.setpos(-500, -200)
+            racer_1.color(self.color)
+            racer_1.speed(self.speed)
+            racer_1.shapesize(stretch_len = self.size, stretch_wid = self.size)
+            racer_2.showturtle()
 
-    def turtle_movement(self):
+        if self.color == "blue":
+            racer_2.penup()
+            racer_2.setpos(-500, 0)
+            racer_2.color(self.color)
+            racer_2.speed(self.speed)
+            racer_2.shapesize(stretch_len = self.size, stretch_wid = self.size)
+            racer_2.showturtle()
+
+        if self.color == "green":
+            racer_3.penup()
+            racer_3.setpos(-500, -100)
+            racer_3.color(self.color)
+            racer_3.speed(self.speed)
+            racer_3.shapesize(stretch_len = self.size, stretch_wid = self.size)
+            racer_3.showturtle()
+
+        if self.color == "red":
+            racer_4.penup()
+            racer_4.setpos(-500, 200)
+            racer_4.color(self.color)
+            racer_4.speed(self.speed)
+            racer_4.shapesize(stretch_len = self.size, stretch_wid = self.size)
+            racer_4.showturtle()
+
+        if self.color == "grey":
+            racer_5.penup()
+            racer_5.setpos(-500, -200)
+            racer_5.color(self.color)
+            racer_5.speed(self.speed)
+            racer_5.shapesize(stretch_len = self.size, stretch_wid = self.size)
+            racer_5.showturtle()
+
+    def movement(self):
         if self.state == 0:
             racer_1.forward(100)
         if self.state == 1:
@@ -65,3 +101,23 @@ class DefineTurtle:
             racer_4.forward(160)
         if self.state == 4:
             racer_5.forward(160)
+
+normal_turtle = DefineTurtle("blue", 6, 1, 100, 0)
+large_turtle = DefineTurtle("black", 3, 3, 150, 1)
+drunk_turtle = DefineTurtle("green", 8, 1, 100, 2)
+ninja_turtle = DefineTurtle("red", 10, 2, 200, 3)
+giga_turtle = DefineTurtle("grey", 10, 3, 300, 4)
+
+normal_turtle.placement()
+large_turtle.placement()
+drunk_turtle.placement()
+ninja_turtle.placement()
+giga_turtle.placement()
+
+normal_turtle.movement()
+large_turtle.movement()
+drunk_turtle.movement()
+ninja_turtle.movement()
+giga_turtle.movement()
+
+window.mainloop()

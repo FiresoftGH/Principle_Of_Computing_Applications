dc83b1f8e702087126dccb725b529c5a06bbcb65
Pattarapark Chutisamoot <most.chuti@gmail.com>
2022-08-31 12:02:19 +0700

    uploading new files

--- Week 5/Source/Lab_5_Pre_Lab.py
+++ Week 5/Source/Lab_5_Pre_Lab.py
@@ -1,4 +1,6 @@
 output = []
+input1 = [1,2,3,4,5]
+
 def mystery1(dummy):
     if len(dummy) == 1: # Base Case
         output.append(dummy[0])
@@ -7,6 +9,7 @@ def mystery1(dummy):
 
     else: # Recursive Case
         a = dummy[0]
+        dummy.pop(0)
         b = mystery1(dummy)
 
         if a > b:
@@ -18,4 +21,5 @@ def mystery1(dummy):
             print("Outputs: ", output)
             return b
 
-mystery1([1,2,3,4,5])
+
+mystery1(input1)

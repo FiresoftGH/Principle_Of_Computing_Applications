da584a505be13992298a483c4ae0653dfbe88423
Pattarapark Chutisamoot <most.chuti@gmail.com>
2022-09-02 23:19:31 +0700

    In_Lab_2.py updated

--- Week 5/Source/In_Lab_2.py
+++ Week 5/Source/In_Lab_2.py
@@ -1,11 +1,11 @@
 import turtle
-debug = []
+data = []
 turtle.penup()
 
 def stars1(n):
-    if n > 1:
+    if n > 0:
         stars = ""
-        for x in range(n - 1):
+        for x in range(n):
             stars += "*"
             turtle.dot(10, "black")
             turtle.fd(20)
@@ -14,12 +14,12 @@ def stars1(n):
         turtle.fd(20)
         turtle.right(-90)
         print(stars)
-        debug.append(stars)
-        # print(debug)
+        data.append(stars)
+        # print(data)
         stars1(n - 1)
     else:
-        debug.reverse()
-        for x in debug:
+        data.reverse()
+        for x in data:
             print(x)
             for x in range(len(x)): 
                 turtle.dot(10, "black")
@@ -29,7 +29,16 @@ def stars1(n):
             turtle.fd(20)
             turtle.right(-90)
         
+# stars1(5)
+convert = {}
+data_2 = []
 
-stars1(5)
-turtle.mainloop()
+def stars2(n):
+    if n > 0:
+        for x in range(n):
+            val += 1
+        return
         
+stars2(5)
+
+turtle.mainloop()

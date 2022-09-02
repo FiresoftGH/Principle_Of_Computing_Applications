8bee802dfb2b2222d820482717976c902c9b8819
Pattarapark Chutisamoot <most.chuti@gmail.com>
2022-09-02 21:31:29 +0700

    In_Lab_2 was created

--- Week 5/Source/In_Lab_2.py
+++ Week 5/Source/In_Lab_2.py
@@ -1,19 +1,30 @@
 import turtle
+debug = []
+
+def control():
+    turtle.dot(10, "black")
+    turtle.penup()
+    # turtle.left(180)
+    turtle.forward(20)
+    turtle.pendown()
+    # turtle.left(-180)
+
 def stars1(n):
-    stars = ""
     if n > 1:
-        # stars = ""
+        stars = ""
         for x in range(n - 1):
             stars += "*"
+            control()
         print(stars)
+        debug.append(stars)
+        # print(debug)
         stars1(n - 1)
-        return
-    elif n == 1:
-        # stars = ""
-        for x in range(n + 1):
-            stars += "*"
-        print(stars)
-        stars1(n - 1)
+    else:
+        debug.reverse()
+        for x in debug:
+            print(x)
+        
 
 stars1(5)
+turtle.mainloop()
         

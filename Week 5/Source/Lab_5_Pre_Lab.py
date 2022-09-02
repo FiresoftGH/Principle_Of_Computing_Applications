2e555e7b397c2736482691c2b40e22b8138e7c88
Pattarapark Chutisamoot <most.chuti@gmail.com>
2022-09-02 09:32:52 +0700

    Lab_5_Pre_Lab was updated

--- Week 5/Source/Lab_5_Pre_Lab.py
+++ Week 5/Source/Lab_5_Pre_Lab.py
@@ -1,30 +1,62 @@
-output = []
-input1 = [1,1,5,5,5,5]
-
-def mystery1(dummy):
-    if len(dummy) == 1: # Base Case
-        output.append(dummy[0])
-        print("Outputs: ", output)
-        return dummy[0]
-
-    else: # Recursive Case
-        a = dummy[0]
-        dummy.pop(0)
-        b = mystery1(dummy)
-
-        if a > b:
-            output.append(a)
-            print("Outputs: ", output)
-            return a
-        else:
-            output.append(b)
-            print("Outputs: ", output)
-            return b
-
-mystery1(input1)
-
-"""
-Outputs the last element of the list with the same number
-as the number of the list
-
-"""
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "ename": "",
+     "evalue": "",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[1;31mCanceled future for execute_request message before replies were done"
+     ]
+    },
+    {
+     "ename": "",
+     "evalue": "",
+     "output_type": "error",
+     "traceback": [
+      "The Kernel crashed while executing code in the the current cell or a previous cell. Please review the code in the cell(s) to identify a possible cause of the failure. Click <a href='https://aka.ms/vscodeJupyterKernelCrash'>here</a> for more info. View Jupyter <a href='command:jupyter.viewOutput'>log</a> for further details."
+     ]
+    }
+   ],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.10.6 64-bit (windows store)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.6"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "7b02c74446cc3340ec73a22c9ebfb594356eb177937d6ca99937a2338bea8788"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}

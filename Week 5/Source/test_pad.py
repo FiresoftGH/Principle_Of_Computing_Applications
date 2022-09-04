ec8340c492f0082fe7392de1e5999ddd2a999275
Pattarapark Chutisamoot <most.chuti@gmail.com>
2022-09-04 19:49:23 +0700

    Updated test_pad.py

--- Week 5/Source/test_pad.py
+++ Week 5/Source/test_pad.py
@@ -4,16 +4,32 @@ turtle.penup()
 result = []
 def stars2(n):
     if n > 0:
+        
         answer = ""
         answer += "*"
         result.append(answer)
+        for x in range(len(result)):
+            turtle.dot(10, "black")
+            turtle.fd(20)
+        turtle.right(90)
+        turtle.fd(20)
+        turtle.goto(0, turtle.ycor())
+        turtle.right(-90)
         print(''.join(result))
         stars2(n - 1)
     else:
+
         result.reverse()
         for x in range(len(result)):
             print(''.join(result))
             result.pop()
+            for y in range(len(result) + 1):
+                turtle.dot(10, "black")
+                turtle.fd(20)
+            turtle.right(90)
+            turtle.fd(20)
+            turtle.goto(0, turtle.ycor())
+            turtle.right(-90)
 
 stars2(5)
 

377b160bbe8125dcf651f1fcd0ba1d7747d4ef32
Pattarapark Chutisamoot <most.chuti@gmail.com>
2022-09-13 18:54:41 +0700

    Removing "Collections" Lib from selected files

--- Week 6/Source/In_lab_2_Stack_Valet_Parking.py
+++ Week 6/Source/In_lab_2_Stack_Valet_Parking.py
@@ -1,5 +1,33 @@
 import random as rd
 import names
+
+class Stack:
+    def __init__(self, data = []):
+        self.data = data
+
+    def __str__(self):
+        return f'Stack is {self.data}'
+
+    def push(self, item):
+        return [item] + self.data
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[0]
+
+    def is_empty(self):
+        if len(self.data) == 0:
+            return True
+        else:
+            return False
+    
+    def size(self):
+        return len(self.data)
+
+parking_lot = Stack([])
+
 class Customer:
     def __init__(self, name, car, lot):
         self.name = name
@@ -9,15 +37,13 @@ class Customer:
     def __str__(self):
         return f'Customer name is {self.name}, your car is {self.car}, you want to park at {self.lot} lane'
 
-parking_lot = []
-
 class Reservation(Customer):
     def __init__(self, name, car, lot):
         super().__init__(name, car, lot)
 
     def get_lot(self):
-        if len(parking_lot) <= 10:
-            parking_lot.append([self.name, self.car])
+        if parking_lot.size() <= 10:
+            parking_lot.push([self.name, self.car])
 
     def get_out(self):
         pass
